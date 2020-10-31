df776c2fe4d619c1457cf74beda9f73781f49d47
Adel de la LLave <kiraitachimodu@gmail.com>
2020-10-31 23:06:08 +0100

    Update 0.4
    Cancel Output

--- HotspotshieldGui.sh
+++ HotspotshieldGui.sh
@@ -1,3 +1,9 @@
 option=`kdialog --combobox "<h1>Welcome to KinuxShield</h1><h4>KinuxShield is an unofficial KDE GUI for Hotspotshield</h4><center><img src='KinuxShield.png'></center><p>Made by kiraitachi. For any issue: <br>https://github.com/kiraitachi/KinuxShield</p><p>Select an option:</p>" "hotspotshield account signin" "hotspotshield account signout" "hotspotshield start" "hotspotshield stop" "hotspotshield connect" "hotspotshield account status" "hotspotshield locations" "hotspotshield status" "hotspotshield disconnect" "curl ipinfo.io"`
 
-kdialog --msgbox "`$option`"
+answer="$?"
+if [ "$answer" = 0 ]; then
+	kdialog --msgbox "`$option`"
+else
+	kdialog --error "Thanks for using KinuxShield by kiraitachi"
+fi;
+
