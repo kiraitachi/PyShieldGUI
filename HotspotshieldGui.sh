7024cdec8be9731b789709c7370047844502456a
Adel de la LLave <kiraitachimodu@gmail.com>
2020-11-01 01:26:30 +0100

    Update 0.6

--- HotspotshieldGui.sh
+++ HotspotshieldGui.sh
@@ -1,4 +1,4 @@
-option=`kdialog --combobox "<h1>Welcome to KinuxShield</h1><h4>KinuxShield is an unofficial KDE GUI for Hotspotshield</h4><center><img src='KinuxShield.png'></center><p>Made by kiraitachi. For any issue: <br>https://github.com/kiraitachi/KinuxShield</p><p>Select an option:</p>" "hotspotshield account signin" "hotspotshield account signout" "hotspotshield start" "hotspotshield stop" "hotspotshield connect" "hotspotshield account status" "hotspotshield locations" "hotspotshield status" "Disconnect VPN" "What's my public IP?"`
+option=`kdialog --combobox "<h1>Welcome to KinuxShield</h1><h4>KinuxShield is an unofficial KDE GUI for Hotspotshield</h4><center><img src='KinuxShield.png'></center><p>Made by kiraitachi. For any issue: <br>https://github.com/kiraitachi/KinuxShield</p><p>Select an option:</p>" "hotspotshield account signin" "hotspotshield account signout" "hotspotshield start" "hotspotshield stop" "hotspotshield connect" "hotspotshield account status" "VPN Locations" "VPN Client Status" "Disconnect VPN" "What's my public IP?"`
 
 answer="$?"
 if [ "$answer" = 0 ] && [ "$option" = "hotspotshield account signin" ]; then
@@ -19,11 +19,11 @@ elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield connect" ]; then
 elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield account status" ]; then
     kdialog --msgbox "`$option`"
 
-elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield locations" ]; then
-    kdialog --msgbox "`$option`"
+elif [ "$answer" = 0 ] && [ "$option" = "VPN Locations" ]; then
+    kdialog --combobox "Select the Location to connect:" "`hotspotshield locations`"
 
-elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield status" ]; then
-    kdialog --msgbox "`$option`"
+elif [ "$answer" = 0 ] && [ "$option" = "VPN Client Status" ]; then
+    kdialog --msgbox "`hotspotshield status`"
 
 elif [ "$answer" = 0 ] && [ "$option" = "Disconnect VPN" ]; then
     kdialog --title "KinuxShield `hotspotshield disconnect`" --passivepopup \ "VPN Disconnected" 10
