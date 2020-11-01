5028ee246d5497447336c3bb864f2a3bf8f69cc9
Adel de la LLave <kiraitachimodu@gmail.com>
2020-11-01 01:13:29 +0100

    Update 0.5
    Major control of each option

--- HotspotshieldGui.sh
+++ HotspotshieldGui.sh
@@ -1,9 +1,36 @@
-option=`kdialog --combobox "<h1>Welcome to KinuxShield</h1><h4>KinuxShield is an unofficial KDE GUI for Hotspotshield</h4><center><img src='KinuxShield.png'></center><p>Made by kiraitachi. For any issue: <br>https://github.com/kiraitachi/KinuxShield</p><p>Select an option:</p>" "hotspotshield account signin" "hotspotshield account signout" "hotspotshield start" "hotspotshield stop" "hotspotshield connect" "hotspotshield account status" "hotspotshield locations" "hotspotshield status" "hotspotshield disconnect" "curl ipinfo.io"`
+option=`kdialog --combobox "<h1>Welcome to KinuxShield</h1><h4>KinuxShield is an unofficial KDE GUI for Hotspotshield</h4><center><img src='KinuxShield.png'></center><p>Made by kiraitachi. For any issue: <br>https://github.com/kiraitachi/KinuxShield</p><p>Select an option:</p>" "hotspotshield account signin" "hotspotshield account signout" "hotspotshield start" "hotspotshield stop" "hotspotshield connect" "hotspotshield account status" "hotspotshield locations" "hotspotshield status" "Disconnect VPN" "What's my public IP?"`
 
 answer="$?"
-if [ "$answer" = 0 ]; then
+if [ "$answer" = 0 ] && [ "$option" = "hotspotshield account signin" ]; then
 	kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield account signout" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield start" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield stop" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield connect" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield account status" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield locations" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "hotspotshield status" ]; then
+    kdialog --msgbox "`$option`"
+
+elif [ "$answer" = 0 ] && [ "$option" = "Disconnect VPN" ]; then
+    kdialog --title "KinuxShield `hotspotshield disconnect`" --passivepopup \ "VPN Disconnected" 10
+
+elif [ "$answer" = 0 ] && [ "$option" = "What's my public IP?" ]; then
+    kdialog --msgbox "`curl ipinfo.io`"
+
 else
 	kdialog --error "Thanks for using KinuxShield by kiraitachi"
 fi;
-
